1bd6e17a26d6a75e9bfb9aa65dd4ffee0c51fd4b
Wira0509 <wirahrpratama@gmail.com>
2025-11-18 13:53:03 +0700

    memperbaiki tampilan

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -62,7 +62,7 @@
                 <span class="sr-only">Buka Filter</span>
             </button>
 
-            <form action="{{ route('film.search') }}" method="GET" class="flex w-full md:w-[32rem] ml-auto"
+            <form action="{{ route('film.search') }}" method="GET" class="w-full md:w-[32rem] ml-auto"
                   x-data="{ isSubmitting: false }"
                   @submit="isSubmitting = true">
                 
@@ -76,26 +76,28 @@
                     @endif
                 @endforeach
 
-                <input type="text" 
-                       name="query" 
-                       placeholder="Cari judul, plot, tahun, atau aktor..."
-                       value="{{ $query ?? '' }}"
-                       class="flex-grow p-3 bg-white border-0 rounded-l-md text-lg text-black focus:outline-none focus:ring-2 focus:ring-imdb-yellow focus:border-transparent">
+                <div class="relative w-full text-gray-600 focus-within:text-gray-400">
+                    
+                    <input type="text" 
+                           name="query" 
+                           placeholder="Cari judul, plot, tahun, atau aktor..."
+                           value="{{ $query ?? '' }}"
+                           class="w-full py-3 pl-4 pr-12 bg-white border-none rounded-md text-lg text-black focus:outline-none focus:ring-2 focus:ring-imdb-yellow placeholder-gray-500">
+                    
+                    <button type="submit" 
+                            class="absolute right-2 top-1/2 transform -translate-y-1/2 p-2 bg-transparent hover:text-imdb-yellow transition-colors disabled:cursor-not-allowed"
+                            :disabled="isSubmitting">
                 
-                <button type="submit" 
-                        class="p-3 bg-imdb-yellow text-black font-bold text-lg rounded-r-md hover:bg-yellow-300
-                               disabled:bg-gray-500 disabled:cursor-not-allowed"
-                :disabled="isSubmitting">
-            
-                    <svg x-show="!isSubmitting" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
+                        <svg x-show="!isSubmitting" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clip-rule="evenodd" />
+                        </svg>
 
-                    <svg x-show="isSubmitting" style="display: none;" class="animate-spin h-6 w-6 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                </button>
+                        <svg x-show="isSubmitting" style="display: none;" class="animate-spin h-6 w-6 text-imdb-yellow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    </button>
+                </div>
             </form>
         </div>
     </header>
